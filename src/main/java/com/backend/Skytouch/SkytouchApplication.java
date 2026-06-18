c6ff6c96d820cd435c9eb6ec9aa5dbb5686cfc75
ochogwuprince92 <ochogwuprince92@gmail.com>
2026-06-18 15:44:46 +0100

    initial project setup

---
+++ src/main/java/com/backend/Skytouch/SkytouchApplication.java
@@ -0,0 +1,13 @@
+package com.backend.Skytouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkytouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkytouchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/Skytouch/SkytouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.Skytouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkytouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
